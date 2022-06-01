9d5e701ed5680214d594dc86007c8da8f1815b55
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-06-01 09:52:34 -0500

    train test validate

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,7 @@
 from acquire_nick import *
 from prepare_nick import *
 import os 
+from sklearn.model_selection import train_test_split
 
 def wrangle_df(use_cache=True): 
     
@@ -14,3 +15,20 @@ def wrangle_df(use_cache=True):
     df = prep_data(acquire_data())
     
     return df
+
+def train_validate_test_split(df):
+    ''' 
+    This function takes in a dataframe and splits it 80:20.  The 20% will be our testing datafrme for our final model.  The 80% will be split a second time (70:30), creating our final training dataframe and a dataframe to validate our model with before testing.  Leaving us we a Train (56%), Validate(24%) and Test (20%) Dataframe from our original data (100%)
+    '''
+    
+    train, test = train_test_split(df, 
+                               train_size = 0.8,
+                               random_state=1313)
+    
+    
+    train, validate = train_test_split(train,
+                                  train_size = 0.7,
+                                  random_state=1313)
+    
+    
+    return train, validate, test
